f4678cd031ed0791eaad7ae145233d7df05c34f2
dispersion01 <anyta1901@gmail.com>
2019-05-05 14:14:54 +0300

    Fix errors

--- src/main/java/ru/nikolaeva/servlet/ImageServlet.java
+++ src/main/java/ru/nikolaeva/servlet/ImageServlet.java
@@ -7,22 +7,29 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ImageServlet extends HttpServlet {
     @Override
-    protected void doGet (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (req.getPathInfo() != null ){
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getPathInfo() != null) {
             String[] split = req.getPathInfo().split("/");
-            if (split.length != 2 ) {
+            if (split.length != 2) {
                 throw new RuntimeException("are you kidding me?");
             }
             var id = split[1];
-            var path = Path.get("").resolve(id);
+
+            var path = Paths.get("C:/Users/Anyta/IdeaProjects/04 Servlet Servlet Nikolaeva/upload").resolve(id);
             if (!Files.exists(path)) {
                 throw new RuntimeException("404");
-                }
-            Files.copy(path,resp.getOutputStream());
+            }
+
+//      resp.setHeader("Content-Type", "text/plain");
+            Files.copy(path, resp.getOutputStream());
+
+//      resp.setHeader("Content-Type", "application/octet-stream");
+//      resp.setHeader("Content-Disposition", "Attachment; filename=exported.csv");
+//      Files.copy(path, resp.getOutputStream());
         }
     }
-
 }
--- src/main/java/ru/nikolaeva/servlet/IndexServlet.java
+++ src/main/java/ru/nikolaeva/servlet/IndexServlet.java
@@ -1,5 +1,6 @@
 package ru.nikolaeva.servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,7 +9,7 @@ import java.io.IOException;
 
 public class IndexServlet extends HttpServlet {
     @Override
-    protected void service (HttpServletRequest req, HttpServletResponse resp) throws SerialException, IOException {
+    protected void service (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getOutputStream().write("ok".getBytes());
 
     }
