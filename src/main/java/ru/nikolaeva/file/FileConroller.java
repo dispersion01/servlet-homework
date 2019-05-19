e82d2d60b757b36268fa8e139c270397b14fbd4c
dispersion01 <anyta1901@gmail.com>
2019-05-19 17:41:13 +0300

    Fix errors

--- src/main/java/ru/nikolaeva/file/FileConroller.java
+++ src/main/java/ru/nikolaeva/file/FileConroller.java
@@ -1,5 +1,11 @@
 package ru.nikolaeva.file;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -7,7 +13,8 @@ import java.util.logging.Logger;
 
 public class FileConroller {
     @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
-    public @ResponseBody String handleFileUpload(@RequestParam("file") MultipartFile file) {
+    public @ResponseBody
+    String handleFileUpload(@RequestParam("file") MultipartFile file) {
 
         if (!file.isEmpty()) {
 
